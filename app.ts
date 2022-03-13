5e3d30db8f1d82c475e845a59ba05d46e916d9de
luissevilla <luissevilla111@gmail.com>
2022-03-13 16:19:31 -0600

    ver 1.0 finished

--- app.ts
+++ app.ts
@@ -4,36 +4,15 @@ import {
   addExtension,
 } from "./shared/repair";
 
-import { readExcel } from "./excelApis/excelApis";
-
-/*const originalNumber = "(33) 1525 7877";
-console.log("original");
-console.log(originalNumber);
-console.log("withoutSpaces");
-const withoutSpaces = removeBlankSpaces(originalNumber);
-console.log(withoutSpaces);
-console.log("withoutBrackets");
-const withoutBrackets = removeBrackets(withoutSpaces);
-console.log(withoutBrackets);
-console.log("addind +52");
-const addingfifthytwo = addExtension(withoutBrackets);
-console.log(addingfifthytwo);*/
+import { readExcel, writeExcel, writeJson } from "./excelApis/excelApis";
 
 const repairPhone = (phoneString: String) => {
   phoneString = phoneString.trim();
 
   if (phoneString === "") return "";
-  //console.log("original");
-  //console.log(phoneString);
-  //console.log("withoutSpaces");
   const withoutSpaces = removeBlankSpaces(phoneString);
-  //console.log(withoutSpaces);
-  //console.log("withoutBrackets");
   const withoutBrackets = removeBrackets(withoutSpaces);
-  //console.log(withoutBrackets);
-  //console.log("addind +52");
   const addingfifthytwo = addExtension(withoutBrackets);
-  //console.log(addingfifthytwo);
 
   return addingfifthytwo;
 };
@@ -42,7 +21,7 @@ const tranformedPhones = (phoneNumbersString: string) => {
   if (phoneNumbersString.length < 0) return "";
 
   const phoneNumbers: string[] = phoneNumbersString.split(",");
-  console.log(phoneNumbers);
+  //console.log(phoneNumbers);
 
   const newPhoneNumbers = phoneNumbers.map((ph) => {
     const nph = repairPhone(ph);
@@ -55,18 +34,21 @@ const tranformedPhones = (phoneNumbersString: string) => {
   return newPhonesJoined;
 };
 
-const path = "data/extension.xlsx";
+const path = "data";
 
 const dataExcel = readExcel(path);
 let i = 0;
-for (const columnns of dataExcel) {
-  const phoneNumbers = columnns.telefonosGmail;
-  //console.log(`i ${i}`);
-  //console.log(phoneNumbers.trim());
-  //i++;
-  console.log(tranformedPhones(phoneNumbers));
-}
-//console.log(dataExcel[165]);
 
-//console.log(tranformedPhones(dataExcel[165].telefonosGmail.trim()));
-//console.log(tranformedPhones("3312949604,(33) 1525 7877"))
+//console.log(dataExcel);
+
+const finalData = dataExcel.map((columns) => {
+  const phoneNumbers = columns.telefonosGmail;
+  const dataTrasformed = tranformedPhones(phoneNumbers);
+
+  return { ...columns, telefonosGmail: dataTrasformed };
+});
+
+console.log(finalData);
+
+writeExcel(path, finalData);
+writeJson(path, finalData);
--- excelApis/excelApis.ts
+++ excelApis/excelApis.ts
@@ -1,8 +1,9 @@
 import XSLX from "xlsx";
 import { dataExcel } from "../interfaces/interfaces";
+import fs from "fs";
 
 export const readExcel = (path: string) => {
-  const workbook = XSLX.readFile(path);
+  const workbook = XSLX.readFile(`${path}/extension.xlsx`);
   const workbookSheet = workbook.SheetNames;
   const sheet = workbookSheet[0];
   const dataExcel = <dataExcel[]>XSLX.utils.sheet_to_json(
@@ -15,4 +16,38 @@ export const readExcel = (path: string) => {
   // console.log(f);
 
   return dataExcel;
+};
+
+export const writeExcel = (path: string, data: dataExcel[]) => {
+  const sheet = XSLX.utils.json_to_sheet(data);
+  const workbook = XSLX.utils.book_new();
+  workbook.SheetNames.push(`sheet1`);
+  workbook.Sheets["sheet1"] = sheet;
+  XSLX.writeFile(workbook, `${path}/extensionRepaired.xlsx`);
+};
+
+export const writeJson = (path: string, data: dataExcel[]) => {
+  try {
+    const JSONData = data.map((da) => {
+      const phoneNumbersString = da.telefonosGmail.split(",");
+
+      const phoneNumbers = phoneNumbersString.map((phoneNum) => {
+        return {
+          type: "Principal",
+          value: phoneNum == "***" ? "" : phoneNum,
+        };
+      });
+
+      return {
+        resourceId: da.IDGMAIL,
+        etag: da.etagGmail,
+        name: da.nombreZoho,
+        phoneNumbers,
+      };
+    });
+    const finalData = JSON.stringify(JSONData);
+    fs.writeFileSync(`${path}/data.json`, finalData);
+  } catch (err) {
+    console.log(`Error Writing JSON FILE ${err}`);
+  }
 };
